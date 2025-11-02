2efce234b1282fcc4d2f3172b0b4906924446fe5
Nazerke Abzhamal <nazerke.ae7@gmail.com>
2025-11-03 01:53:33 +0500

    feat: add Main entrypoint for CLI execution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.util.PerformanceAnalyzer;
+import org.example.util.Metrics;
+import org.example.io.GraphIO;
+import org.example.io.Graph;
+import org.example.scc.KosarajuSCC;
+import org.example.topo.CondensationGraph;
+import org.example.topo.TopologicalSort;
+import org.example.dagsp.DagLongestPaths;
+import java.util.List;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            PerformanceAnalyzer.runAll();
+            return;
         }
+
+        String path = args[0];
+        System.out.println("Dataset: " + path);
+        GraphIO.LoadResult lr = GraphIO.loadFromFile(path);
+        Graph g = lr.graph;
+
+        Metrics m1 = new Metrics(); m1.start();
+        var sccs = new KosarajuSCC(g, m1).computeSCCs();
+        m1.stop();
+        System.out.println("SCC count = " + sccs.size() + "  " + m1);
+
+        int[] compId = KosarajuSCC.buildComponentIds(sccs, g.size());
+        var dag = new CondensationGraph(g, compId, sccs.size()).getCondensationGraph();
+
+        Metrics m2 = new Metrics(); m2.start();
+        List<Integer> topo = new TopologicalSort(dag).sort();
+        m2.stop();
+        System.out.println("Topo order = " + topo + "  " + m2);
+
+        Metrics m3 = new Metrics(); m3.start();
+        var longest = new DagLongestPaths(dag, m3).longestFrom(compId[lr.source], topo);
+        m3.stop();
+
+        long best = Long.MIN_VALUE;
+        for (long d : longest.dist) best = Math.max(best, d);
+        System.out.println("Critical (longest) path weight = " + (best==Long.MIN_VALUE? "N/A": best) + "  " + m3);
     }
 }
